71fa2fa9a7d68961bbef9803a01dfc50d76b905b
Aruna Lakmal <arunarandunu@gmail.com>
2019-11-01 10:36:07 +0530

    Setting up private subnets

--- main.tf
+++ main.tf
@@ -60,4 +60,40 @@ resource "aws_subnet" "tc_public1_subnet" {
   }
 }
 
+resource "aws_subnet" "tc_public2_subnet" {
+  vpc_id = "${aws_vpc.tc_vpc.id}"
+  cidr_block = "${var.cidrs["public2"]}"
+  map_public_ip_on_launch = true
+  availability_zone = "${data.aws_availability_zones.available.names[1]}"
+
+  tags = {
+      Name = "tc_public_subnet2"
+  }
+}
+
+resource "aws_subnet" "tc_private1_subnet" {
+  vpc_id = "${aws_vpc.tc_vpc.id}"
+  cidr_block = "${var.cidrs["private1"]}"
+  map_public_ip_on_launch = false
+  availability_zone = "${data.aws_availability_zones.available.names[0]}"
+
+  tags = {
+      Name = "tc_private_subnet1"
+  }
+}
+
+resource "aws_subnet" "tc_private2_subnet" {
+  vpc_id = "${aws_vpc.tc_vpc.id}"
+  cidr_block = "${var.cidrs["private2"]}"
+  map_public_ip_on_launch = false
+  availability_zone = "${data.aws_availability_zones.available.names[1]}"
+
+  tags = {
+      Name = "tc_private_subnet2"
+  }
+}
+
+
+
+
 
