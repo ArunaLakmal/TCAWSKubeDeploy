a209854c92e6d51b15a971dab49a49ecbd85edda
Aruna Lakmal <arunarandunu@gmail.com>
2019-11-02 17:58:55 +0530

    adding private

--- main.tf
+++ main.tf
@@ -144,3 +144,48 @@ resource "aws_security_group" "tc_kubeadm_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
+
+#---- Public Security Group ----
+
+resource "aws_security_group" "tc_public_sg" {
+  name = "tc_public_sg"
+  description = "Security Group for the Public instances"
+  vpc_id = "${aws_vpc.tc_vpc.id}"
+
+#---- HTTP Allow ----
+  ingress {
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = "[0.0.0.0/0]"
+  }
+
+  egress {
+    from_port = 0 
+    to_port = 0
+    protocol = -1
+    cidr_blocks = "[0.0.0.0/0]"
+  }
+}
+
+resource "aws_security_group" "tc_private_sg" {
+  name = "tc_private_sg"
+  description = "Security Group for Private instances"
+  vpc_id = "${aws_vpc.tc_vpc.id}"
+
+#---- Access From VPC ----
+  ingress {
+    from_port = 0
+    to_port = 0
+    protocol = -1
+    cidr_blocks = ["${var.vpc_cidr}"]
+  }
+
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = -1
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
