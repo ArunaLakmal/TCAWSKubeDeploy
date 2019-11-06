174f85e844588d89169b0c276eaa1433378e159b
Aruna Lakmal <arunarandunu@gmail.com>
2019-11-07 00:58:11 +0530

    restore

--- main.tf
+++ main.tf
@@ -129,15 +129,6 @@ resource "aws_security_group" "tc_kubeadm_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  #---- 8080 Allow ----
-
-  ingress {
-    from_port   = 8080
-    to_port     = 8080
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
   #---- HTTP Allow ----
   ingress {
     from_port   = 80
